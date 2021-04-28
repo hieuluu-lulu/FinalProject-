5db37307a3837b358a884e6cdc4743101ed73a50
hieuluu-lulu <hieulvtgcd17213@fpt.edu.vn>
2021-04-29 01:08:09 +0700

    'disable'

--- src/routes/site.js
+++ src/routes/site.js
@@ -3,7 +3,7 @@ const route = express.Router();
 const siteController = require('../app/controllers/SiteController');
 const { ensureAuth } = require('../config/auth');
 const { forwardAuth } = require('../config/auth');
-route.get('', forwardAuth, siteController.index);
+route.get('/', forwardAuth, siteController.index);
 route.get('/dashboard', ensureAuth, siteController.indexDashboard);
 route.get('/discuss',siteController.pageNotFound);
 route.get('/contact',siteController.pageNotFound);
