9590f8ac2d1309d0a0e000d0a4b571de5902fa91
hieuluu-lulu <hieulvtgcd17213@fpt.edu.vn>
2021-04-28 21:00:27 +0700

    master 1

--- src/routes/site.js
+++ src/routes/site.js
@@ -3,6 +3,6 @@ const route = express.Router();
 const siteController = require('../app/controllers/SiteController');
 const { ensureAuth } = require('../config/auth');
 const { forwardAuth } = require('../config/auth');
-route.get('/', forwardAuth, siteController.index);
+route.get('', forwardAuth, siteController.index);
 route.get('/dashboard', ensureAuth, siteController.indexDashboard);
 module.exports = route;
