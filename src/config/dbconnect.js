a5baeaa603593d4a08ad8c7642791ace347cf3ee
hieuluu-lulu <hieulvtgcd17213@fpt.edu.vn>
2021-04-29 17:09:12 +0700

    fix

--- src/config/dbconnect.js
+++ src/config/dbconnect.js
@@ -24,15 +24,6 @@ async function connect() {
             },
         );
 
-        await mongoose.connect(
-            'mongodb+srv://hieuluu:hieuluu123@cluster0.uzxq5.mongodb.net/f8_education_dev?retryWrites=true&w=majority',
-            {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-                useCreateIndex: true,
-            },
-        );
-
         console.log('Connection Sucessfully!');
     } catch (error) {
         console.log('Connection Failure!');
